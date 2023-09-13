dc8b85a2e485e6f4c7c13e17ca1879d61f0ca4dd
Divy Solanki <divysolanki7@gmail.com>
2023-09-13 18:30:40 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from cnn_chicken_disease_classifier import logger
 from cnn_chicken_disease_classifier.pipeline.stage_01_data_ingestion import (
     DataIngestionTrainingPipeline,
 )
+from cnn_chicken_disease_classifier.pipeline.stage_02_prepare_base_model import (
+    PrepareBaseModelTrainingPipeline,
+)
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -10,6 +13,18 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Prepare base Model"
+
+try:
+    logger.info(f"*************************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
---
+++ src/cnn_chicken_disease_classifier/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,30 @@
+from cnn_chicken_disease_classifier.config.configuration import ConfigurationManager
+from cnn_chicken_disease_classifier.components.prepare_base_model import (
+    PrepareBaseModel,
+)
+from cnn_chicken_disease_classifier import logger
+
+STAGE_NAME = "Prepare base Model"
+
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
